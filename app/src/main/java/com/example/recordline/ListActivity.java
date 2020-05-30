68fcf2051d324fd789d85532b89823115a4bb1ce
jemimahmartinez <martinezjemimah@gmail.com>
2020-05-30 21:18:59 +1200

    update activity reference

--- app/src/main/java/com/example/recordline/ListActivity.java
+++ app/src/main/java/com/example/recordline/ListActivity.java
@@ -1,4 +1,14 @@
 package com.example.recordline;
 
-public class ListActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
 }

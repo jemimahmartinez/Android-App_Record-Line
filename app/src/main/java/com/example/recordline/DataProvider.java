095a89f263a8aefee989cb9fc367234129b0dab3
jemimahmartinez <martinezjemimah@gmail.com>
2020-06-06 00:00:45 +1200

    added rock album images and refactored dataprovided file for rock data. might need to rename files

--- app/src/main/java/com/example/recordline/DataProvider.java
+++ app/src/main/java/com/example/recordline/DataProvider.java
@@ -9,16 +9,17 @@ public class DataProvider {
     public static Map<Integer, String> generateAlbumList() {
         Map<Integer, String> albums =
                 new LinkedHashMap<Integer, String>();
-        albums.put(1, "Album name");
-        albums.put(2, "Album name");
-        albums.put(3, "Album name");
-        albums.put(4, "Album name");
-        albums.put(5, "Album name");
-        albums.put(6, "Album name");
-        albums.put(7, "Album name");
-        albums.put(8, "Album name");
-        albums.put(9, "Album name");
-        albums.put(10, "Album name");
+        //key, album name
+        albums.put(1, "Sgt. Pepper's Lonely Hearts Club Band"); //The Beatles
+        albums.put(2, "Blood On The Tracks"); //Bob Dylan
+        albums.put(3, "Exile On Main St."); //The Rolling Stones
+        albums.put(4, "Are You Experienced?"); //The Jimi Hendrix Experience
+        albums.put(5, "Born To Run"); //Bruce Springsteen
+        albums.put(6, "Lead Zeppelin IV"); //Led Zeppelin
+        albums.put(7, "Never Mind The Bollocks, Here's The Sex Pistols"); //Sex Pistols
+        albums.put(8, "Who's Next"); //The Who
+        albums.put(9, "Everybody Knows This is Nowhere"); //Neil Young
+        albums.put(10, "The Wall"); //Pink Floyd
         return albums;
     }
 
@@ -28,7 +29,7 @@ public class DataProvider {
         for (Integer key : albums.keySet()) {
             int digit = key;
             String albumName = albums.get(key);
-            String vinyl = "vinyl"+String.valueOf(key);
+            String vinyl = "album_rock"+String.valueOf(key)+"_1";
             Album n = new Album(digit, albumName, vinyl);
             albumList.add(n);
         }

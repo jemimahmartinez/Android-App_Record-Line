05a22a8cb952b657bf4b8fa6a7b6bf159c06309d
nbed807 <nbed807@aucklanduni.ac.nz>
2020-06-09 00:26:35 +1200

    Enabled on click navigation to list view Search bar layout set up

--- app/src/main/java/com/example/recordline/MainActivity.java
+++ app/src/main/java/com/example/recordline/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.recordline;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.SearchView;
 
 import java.util.List;
 
@@ -14,5 +21,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView rockCardView = (CardView) findViewById(R.id.activity_main_rock);
+        rockCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent rockIntent = new Intent(getBaseContext(), ListActivity.class);
+                startActivity(rockIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        MenuItem searchItem = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/recordline/SearchResultActivity.java
@@ -0,0 +1,28 @@
+package com.example.recordline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SearchResultActivity extends AppCompatActivity {
+
+    TextView mTextViewSearchResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+        mTextViewSearchResult = findViewById(R.id.textViewSearchResult);
+        if (Intent.ACTION_SEARCH.equals(getIntent().getAction())) {
+            handleSearch(getIntent().getStringExtra(SearchManager.QUERY));
+
+        }
+    }
+
+    private void handleSearch(String searchQuery) {
+        mTextViewSearchResult.setText(searchQuery);
+    }
+}
